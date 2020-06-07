67600db57b5d13eb10862372f5d731f631f9ca72
Vlad Shcherban <vlad@coronalabs.com>
2020-06-07 08:30:51 +0300

    generating from json, not from repos

--- generate.py
+++ generate.py
@@ -1,51 +1,26 @@
 #!/usr/bin/env python3
-import json, os, sys, glob
+import json
+import sys
 
-def getDirs(root):
-    for (_, dirs, _) in os.walk(root):
-        return filter(lambda d: not d.startswith('.'), dirs)
-    return []
 
-def addPluginEntries(pluginDir, entries):
-    verRoot = os.path.join(pluginDir, 'plugins')
-    release = ""
-    try:
-        with open(os.path.join(verRoot, 'RELEASE'), 'r') as f:
-            release = f.read().strip()
-    except:
-        pass
-    versions = {}
-    for ver in getDirs(verRoot):
-        verDir = os.path.join(verRoot, ver)
-        platforms = list(sorted(p for p in getDirs(verDir) if glob.glob(os.path.join(verDir, p, '*'))))
-        if platforms:
-            versions[ver] = platforms
-    if versions and release:
-        entries["r"]=release
-        entries["v"]=versions
+def add_plugin(plugin_json, c):
+    with open(plugin_json, "r", encoding='utf-8') as f:
+        plugin = json.load(f)
+    if not plugin:
+        print("ERROR: unable to read from", plugin_json, file=sys.stderr)
+    c = c.setdefault(plugin["ghAccount"] or '-', {})
+    c = c.setdefault(plugin["publisherId"], {})
+    c = c.setdefault(plugin["plugin"], {})
+    if "latest" in plugin:
+        c["r"] = plugin["latest"]
+    if "supported" in plugin:
+        c["v"] = plugin["supported"]
+    if "error" in plugin:
+        c["e"] = plugin["error"]
 
-def addPlugins(root, plugins):
-    for github_account in getDirs(root):
-        account_plugins = {}
-        account_root = os.path.join(root, github_account)
-        for provider in getDirs(account_root):
-            providerDir = os.path.join(account_root, provider)
-            providerPlugins = {}
-            for plugin in getDirs(providerDir):
-                pluginDir = os.path.join(providerDir, plugin)
-                pluginEntries = {}
-                addPluginEntries(pluginDir, pluginEntries)
-                if pluginEntries:
-                    providerPlugins[plugin] = pluginEntries
-            if providerPlugins:
-                account_plugins[provider] = providerPlugins
-        if account_plugins:
-            plugins[github_account] = account_plugins
 
 if __name__ == "__main__":
-    root = sys.argv[1] if len(sys.argv)==2 else os.path.dirname(os.path.abspath(__file__))
-    print("Scanning for plugins in '{}'".format(root), file=sys.stderr)
     plugins = {}
-    addPlugins(root, plugins)
+    for plugin in sys.argv[1:]:
+        add_plugin(plugin, plugins)
     print(json.dumps(plugins, sort_keys=True, indent=0))
-
--- generate.sh
+++ generate.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 set -e
-git submodule --quiet foreach 'git fetch --quiet --depth=1 origin +refs/tags/*:refs/tags/* || echo error with $sm_path'
-git submodule --quiet foreach 'git describe --tags --abbrev=0 > plugins/RELEASE || echo Release error with $sm_path'
 mkdir -p html
-./generate.py plugins > html/plugins.json
-git submodule --quiet foreach 'rm -f plugins/RELEASE || true'
+./generate.py plugins/*.json > html/plugins.json
